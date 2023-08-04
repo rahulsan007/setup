4e375c3a0968edfb9d44bdb12fa6e3137de2c426
rahulsan007 <santrarahul7070@gmail.com>
2023-08-04 15:49:39 +0530

    envSetup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 class="text-3xl text-orange-600 font-bold underline">
+        Tailwind Installed!
+      </h1>{" "}
+      <h1 class="text-3xl font-bold underline">React Router Dom Installed!</h1>
+      <h1 class="text-3xl font-bold underline">React Icons Installed!</h1>
+    </>
   );
-}
+};
 
 export default App;
