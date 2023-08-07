b18f2bee1da6181e471bece1b4f11159b9a6ae50
Vikas Nayak <vikasnayak96999@gmail.com>
2023-08-07 14:38:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <>
       <h1 class="text-3xl text-orange-600 font-bold underline">
-        Tailwind Installed!
+        Tailwind Installed!bbb
       </h1>{" "}
       <h1 class="text-3xl font-bold underline">React Router Dom Installed!</h1>
       <h1 class="text-3xl font-bold underline">React Icons Installed!</h1>
